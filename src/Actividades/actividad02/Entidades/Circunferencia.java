051a33923939312d9a4590e79780a99870f55487
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-21 12:06:07 -0300

    Inicio de actividad 2

---
+++ src/Actividades/actividad02/Entidades/Circunferencia.java
@@ -0,0 +1,22 @@
+package Actividades.actividad02.Entidades;
+
+public class Circunferencia {
+	private int radio;
+
+    public Circunferencia(int radio) {
+        this.radio = radio;
+    }
+
+
+	public Circunferencia() {
+	}
+
+
+	public int getRadio() {
+		return radio;
+	}
+
+	public void setRadio(int radio) {
+		this.radio = radio;
+	}
+}
---
+++ src/Actividades/actividad02/Servicios/CircunferenciaServicios.java
@@ -0,0 +1,21 @@
+package Actividades.actividad02.Servicios;
+
+import Actividades.actividad02.Entidades.Circunferencia;
+
+public class CircunferenciaServicios {
+	public Circunferencia crearCircunferencia(int radioC){
+		return new Circunferencia(radioC);
+	}
+
+	public double area(int radioC){
+		double areaTotal = radioC * Math.PI;
+		System.out.println("El area de la circunferencia es de: " + areaTotal);
+		return areaTotal;
+	}
+
+	public double perimetro(int radioC){
+        double perimetroTotal = 2 * Math.PI * radioC;
+        System.out.println("El perimetro de la circunferencia es de: " + perimetroTotal);
+        return perimetroTotal;
+    }
+}
