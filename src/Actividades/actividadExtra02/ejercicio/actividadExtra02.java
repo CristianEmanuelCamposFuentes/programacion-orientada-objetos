cf6e0613ca181d3448661df314d9afe870819dd3
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-22 12:55:03 -0300

    Inicio de actividad Extra 2

---
+++ src/Actividades/actividadExtra02/ejercicio/actividadExtra02.java
@@ -0,0 +1,12 @@
+package Actividades.actividadExtra02.ejercicio;
+
+import java.util.Scanner;
+
+public class actividadExtra02 {
+	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+
+
+
+    }
+}
