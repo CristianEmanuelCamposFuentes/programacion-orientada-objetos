796ef2b478b0917f12388222e4dc1e7314e262ea
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-24 11:15:38 -0300

    Optimizacion de validaciones de crearPersona

--- src/Actividades/actividad07/Servicios/PersonaServicios.java
+++ src/Actividades/actividad07/Servicios/PersonaServicios.java
@@ -17,75 +17,79 @@ public class PersonaServicios {
 		System.out.println("Ingrese el nombre: ");
 		String nombre = scanner.nextLine();
 
-		//Validar la edad
-		edadStr = validarEdad(scanner);
-		int edad = Integer.parseInt(String.valueOf(edadStr));
+		// Validar la edad
+		int edad = validarEdad(scanner);
 
 		// Validar el sexo
-		sexoStr = validarSexo(scanner);
-		char sexo = sexoStr;
+		char sexo = validarSexo(scanner);
 
 		// Validar el peso
-		System.out.println("Ingrese el peso: ");
-		pesoStr = Double.parseDouble(validarPeso(scanner));
-		double peso = Double.parseDouble(String.valueOf(pesoStr));
+		double peso = validarPeso(scanner);
 
 		// Validar la altura
-        System.out.println("Ingrese la altura: ");
-		alturaStr = Double.parseDouble(validarAltura(scanner));
-		double altura = Double.parseDouble(String.valueOf(alturaStr));
+		double altura = validarAltura(scanner);
 
 		return new Persona(nombre,edad,sexo,peso,altura);
 	}
 
 	public int validarEdad(Scanner scanner){
-		System.out.println("Ingrese la edad entre 1 y 90: ");
-		String edadStr = scanner.nextLine();
-		int edad = Integer.parseInt(edadStr);
-		if(edad < 0 || edad > 90){
-            System.out.println("La edad debe ser mayor a cero y menor que noventa. Reintentar.");
-			scanner.next(); // Consumir el salto de línea
+		int edad;
 
-			return validarEdad(scanner);
-        }
-        return edad;
+		do {
+			System.out.println("Ingrese la edad entre 1 y 90: ");
+			edad = scanner.nextInt();
+
+			if (edad < 1 || edad > 90) {
+				System.out.println("La edad debe estar entre 1 y 90. Reintentar.");
+			}
+		} while (edad < 1 || edad > 90);
+
+		return edad;
 	}
 
 	public char validarSexo(Scanner scanner){
-		System.out.println("Ingrese el sexo (M) Masculino, (F) Femenino, (O) Otro: ");
-		char sexoStr = scanner.nextLine().toUpperCase().charAt(0);
+		char sexo;
 
-		if((sexoStr != 'M') && (sexoStr != 'F') && (sexoStr != 'O')){
-            System.out.println("El sexo debe ser M, F o O. Reintentar.");
-			scanner.nextLine(); // Consumir el salto de línea
+		do {
+			System.out.println("Ingrese el sexo (M) Masculino, (F) Femenino, (O) Otro: ");
+			sexo = scanner.next().toUpperCase().charAt(0);
 
-			return validarSexo(scanner);
-        }
-        return sexoStr;
+			if (sexo != 'M' && sexo != 'F' && sexo != 'O') {
+				System.out.println("El sexo debe ser M, F o O. Reintentar.");
+			}
+		} while (sexo != 'M' && sexo != 'F' && sexo != 'O');
+
+		return sexo;
 	}
 
-	public String validarPeso(Scanner scanner){
-		System.out.println("Ingrese el peso: ");
-		String pesoStr = scanner.next();
-		double peso = Double.parseDouble(pesoStr);
-		if(peso < 0){
-            System.out.println("El peso debe ser mayor a cero. Reintentar.");
-			scanner.nextInt(); // Consumir el salto de línea
-            return validarPeso(scanner);
-        }
-        return pesoStr;
+
+	public double validarPeso(Scanner scanner){
+		double peso;
+
+		do {
+			System.out.println("Ingrese el peso: ");
+			peso = scanner.nextDouble();
+
+			if (peso < 0) {
+				System.out.println("El peso debe ser mayor a cero. Reintentar.");
+			}
+		} while (peso < 0);
+
+		return peso;
 	}
 
-    public String validarAltura(Scanner scanner){
-	    System.out.println("Ingrese la altura: ");
-	    String alturaStr = scanner.next();
-	    double altura = Double.parseDouble(alturaStr);
+    public double validarAltura(Scanner scanner){
+	    double altura;
 
-	    if(altura < 0){
-            System.out.println("La altura debe ser mayor a cero. Reintentar.");
-			scanner.nextDouble(); // Consumir el salto de línea
-            return validarAltura(scanner);
-        }
-		return alturaStr;
+	    do {
+		    System.out.println("Ingrese la altura: ");
+		    altura = scanner.nextDouble();
+
+		    if (altura < 0) {
+			    System.out.println("La altura debe ser mayor a cero. Reintentar.");
+		    }
+	    } while (altura < 0);
+
+	    return altura;
     }
 }
