c495f79125dbcc0772f5af8133033fbac29e7385
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-24 21:08:48 -0300

    Agregar calculos de promedios al array Personas

--- src/Actividades/actividad07/Servicios/PersonaServicios.java
+++ src/Actividades/actividad07/Servicios/PersonaServicios.java
@@ -28,6 +28,8 @@ public class PersonaServicios {
 
 		// Validar la altura
 		double altura = validarAltura(scanner);
+		// Limpiar el buffer de memoria del scanner
+		scanner.nextLine();
 
 		return new Persona(nombre,edad,sexo,peso,altura);
 	}
@@ -92,4 +94,79 @@ public class PersonaServicios {
 
 	    return altura;
     }
+
+	public int calcularIMC(Persona persona){
+		double peso = persona.getPeso();
+        double altura = persona.getAltura();
+
+        double imc = peso / (altura * altura);
+
+        System.out.println("El IMC es: " + imc);
+		if (imc < 20){
+			System.out.println("La persona está por debajo de su peso ideal.");
+			return -1;
+		} else if(imc >= 20 && imc <= 25){
+			System.out.println("La persona está en su peso ideal.");
+            return 0;
+		} else {
+			System.out.println("La persona tiene sobrepeso.");
+            return 1;
+		}
+	}
+
+	public boolean esMayorDeEdad(Persona persona){
+		int edad = persona.getEdad();
+		// Si se cumple, devuelve verdadero, sino falso, es una comparacion
+		return edad >= 18;
+	}
+
+	// Array de personas para facilitar los calculos
+	public void calcularPromedios(Persona[] personas){
+		double sumaIMC = 0;
+		int cantPersonas = personas.length;
+		int cantPesoIdeal = 0;
+		int cantSobrepeso = 0;
+
+		// Foreach para recorrer cada elemento del array
+		for(Persona persona : personas){
+			int imc = calcularIMC(persona);
+
+            if (imc == 0){
+	            cantPesoIdeal++;
+            } else if (imc == 1){
+	            cantSobrepeso++;
+            }
+
+			sumaIMC += imc;
+		}
+		double promedioIMC = sumaIMC / cantPersonas;
+		double promedioSobrepeso = (double) cantSobrepeso / cantPersonas * 100;
+		double promedioPesoIdeal = (double) cantPesoIdeal / cantPersonas * 100;
+
+		System.out.println("Promedio de IMC: " + promedioIMC);
+		System.out.println("Porcentaje de personas con Sobrepeso: " + promedioSobrepeso);
+		System.out.println("Porcentaje de personas con Peso ideal: " + promedioPesoIdeal);
+	}
+
+	public void calcularMayoresMenores(Persona[] personas){
+		int cantMayores = 0;
+		int cantMenores = 0;
+		int cantPersonas = personas.length;
+
+
+		for(Persona persona : personas){
+			if (esMayorDeEdad(persona)){
+                cantMayores++;
+            } else {
+                cantMenores++;
+            }
+		}
+		System.out.println("Cantidad de personas mayores de edad: " + cantMayores);
+		System.out.println("Cantidad de personas menores de edad: " + cantMenores);
+
+		double porcentajeMayores = (double) cantMayores / cantPersonas * 100;
+		double porcentajeMenores = (double) cantMenores / cantPersonas * 100;
+		System.out.println("Porcentaje de personas mayores de edad: " + porcentajeMayores);
+		System.out.println("Porcentaje de personas menores de edad: " + porcentajeMenores);
+	}
 }
