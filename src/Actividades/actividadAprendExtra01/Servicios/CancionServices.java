173e7a909310fc09a15d4e1d7611dc8b8453c694
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-05 22:08:14 -0300

    Agregar patrones al Scanner

--- src/Actividades/actividadAprendExtra01/Servicios/CancionServices.java
+++ src/Actividades/actividadAprendExtra01/Servicios/CancionServices.java
@@ -5,8 +5,15 @@ import Actividades.actividadAprendExtra01.Entidades.Cancion;
 import java.util.Scanner;
 
 public class CancionServices {
-	Scanner lector = new Scanner(System.in);
+	Scanner lector = new Scanner(System.in).useDelimiter("\n");
 	public Cancion crearCancion() {
+		System.out.println("Ingrese el titulo de la cancion: ");
+		String nombreCancion = lector.nextLine();
 
+		System.out.println("Ingrese el nombre del autor: ");
+		String nombreAutor = lector.nextLine();
+
+		System.out.println("Cancion creada correctamente...");
+		return new Cancion(nombreCancion, nombreAutor);
 	}
 }
--- src/Actividades/actividadAprendExtra01/ejercicio/actividadAprendExtra01.java
+++ src/Actividades/actividadAprendExtra01/ejercicio/actividadAprendExtra01.java
@@ -1,10 +1,23 @@
 package Actividades.actividadAprendExtra01.ejercicio;
+
+import Actividades.actividadAprendExtra01.Entidades.Cancion;
+import Actividades.actividadAprendExtra01.Servicios.CancionServices;
+
 //1. Desarrollar una clase Cancion con los siguientes atributos: titulo y autor. Se deberá́
 //		definir además dos constructores: uno vacío que inicializa el titulo y el autor a cadenas
 //		vacías y otro que reciba como parámetros el titulo y el autor de la canción. Se deberán
 //		además definir los métodos getters y setters correspondientes.
 public class actividadAprendExtra01 {
 public static void main(String[] args){
+	System.out.println("Inicio del programa: " );
+	// Instancia de los servicios
+	CancionServices CS = new CancionServices();
 
+	// Creamos el objeto cancion
+	Cancion cancionNueva = CS.crearCancion();
+
+	System.out.println(cancionNueva);
+
+	System.out.println("Fin del programa.");
 	}
 }
