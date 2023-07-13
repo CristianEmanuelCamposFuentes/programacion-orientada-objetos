bf64ce73a11a9228a7e22f6eccf2105d00010393
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-12 22:47:07 -0300

    Implementar una instancia del juego Ahorcado

--- src/Actividades/actividadAprendExtra06/Servicios/AhorcadoServices.java
+++ src/Actividades/actividadAprendExtra06/Servicios/AhorcadoServices.java
@@ -18,4 +18,57 @@ public class AhorcadoServices {
 		return new Ahorcado(palabraBuscada, letrasEncontradas, cantJugadasMax);
 	}
 
+
+	public void longitud(Ahorcado juego) {
+		System.out.println("Longitud de la palabra: " + juego.getPalabraBuscada().length);
+	}
+
+	public void buscar(Ahorcado juego, String letra) {
+		boolean encontrada = false;
+		for (String l : juego.getPalabraBuscada()) {
+			if (l.equals(letra)) {
+				encontrada = true;
+				break;
+			}
+		}
+		if (encontrada) {
+			System.out.println("Mensaje: La letra pertenece a la palabra");
+		} else {
+			System.out.println("Mensaje: La letra no pertenece a la palabra");
+		}
+	}
+
+	public void encontradas(Ahorcado juego, String letra) {
+		int encontradas = 0;
+		int faltantes = 0;
+		for (String l : juego.getPalabraBuscada()) {
+			if (l.equals(letra)) {
+				encontradas++;
+			} else {
+				faltantes++;
+			}
+		}
+		System.out.println("Número de letras (encontradas, faltantes): (" + encontradas + "," + faltantes + ")");
+	}
+
+	public void intentos(Ahorcado juego) {
+		int oportunidadesRestantes = juego.getCantJugadasMax() - juego.getLetrasEncontradas();
+		System.out.println("Número de oportunidades restantes: " + oportunidadesRestantes);
+	}
+
+	public void juego(Ahorcado juego) {
+		while (juego.getLetrasEncontradas() < juego.getCantJugadasMax()) {
+			System.out.println("----------------------------------------------");
+			System.out.println("Ingrese una letra:");
+			String letra = lector.next();
+
+			buscar(juego, letra);
+			encontradas(juego, letra);
+			intentos(juego);
+			juego.setLetrasEncontradas(juego.getLetrasEncontradas() + 1);
+		}
+		System.out.println("----------------------------------------------");
+		System.out.println("Mensaje: Lo sentimos, no hay más oportunidades");
+	}
 }
+
