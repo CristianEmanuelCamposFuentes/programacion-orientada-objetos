03feaa5e23da415670047d236e6bb0645840ff0f
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-18 21:11:57 -0300

    Agregado test deberiaCalcularArea y deberiaCalcularPerimetro

--- src/JUnit/EjemploVideo1/Servicios/RectanguloServicios.java
+++ src/JUnit/EjemploVideo1/Servicios/RectanguloServicios.java
@@ -1,4 +1,13 @@
 package JUnit.EjemploVideo1.Servicios;
 
+import JUnit.EjemploVideo1.Entidades.Rectangulo;
+
 public class RectanguloServicios {
+	public double calcularArea(Rectangulo rectangulo ) {
+		return rectangulo.getAncho() * rectangulo.getLargo();
+	}
+
+	public double calcularPerimetro(Rectangulo rectangulo){
+		return 2 * (rectangulo.getAncho() + rectangulo.getLargo());
+	}
 }
--- src/JUnit/EjemploVideo1/Test/test/RectanguloTest.java
+++ src/JUnit/EjemploVideo1/Test/test/RectanguloTest.java
@@ -1,13 +1,17 @@
 package JUnit.EjemploVideo1.Test.test;
 
 import JUnit.EjemploVideo1.Entidades.Rectangulo;
+import JUnit.EjemploVideo1.Servicios.RectanguloServicios;
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class RectanguloTest {
 	public RectanguloTest(){
 	}
+
+	RectanguloServicios RS;
 
 	@BeforeClass
 	public static void setUpClass(){
@@ -19,6 +23,7 @@ public class RectanguloTest {
 
 	@Before
 	public void setUp(){
+		RS = new RectanguloServicios();
 	}
 
 	@After
@@ -29,5 +34,19 @@ public class RectanguloTest {
 	public void deberiaInicializarConColor(){
 		// Este metodo dice que no debe ser null para devolver true
 		assertNotNull(new Rectangulo(10,10).getColor());
+	}
+
+	@Test
+	public void deberiaCalcularArea(){
+		assertEquals(100, RS.calcularArea(new Rectangulo(10,10)), 0);
+		assertEquals(20, RS.calcularArea(new Rectangulo(4,5)), 0);
+		assertEquals(1, RS.calcularArea(new Rectangulo(1,1)), 0);
+	}
+
+	@Test
+	public void deberiaCalcularPerimetro(){
+		assertEquals(40, RS.calcularPerimetro(new Rectangulo(10,10)),0);
+		assertEquals(100, RS.calcularPerimetro(new Rectangulo(20,30)),0);
+		assertEquals(30, RS.calcularPerimetro(new Rectangulo(5,10)),0);
 	}
 }
