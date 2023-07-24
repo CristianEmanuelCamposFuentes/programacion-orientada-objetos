0bb7cb68743c38d41503e3d221126a4aa414e041
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-24 00:45:46 -0300

    Agregar metodos en ArrayList

--- src/Colecciones/Videos/Video1/mascotaApp/MascotaApp.java
+++ src/Colecciones/Videos/Video1/mascotaApp/MascotaApp.java
@@ -1,9 +1,12 @@
 package Colecciones.Videos.Video1.mascotaApp;
 
+import java.util.ArrayList;
+
 public class MascotaApp {
 	public static void main(String[] args) {
 		String[]nombresArray = new String[5];
 
+		// Arrays
 		for (int i = 0; i < nombresArray.length; i++){
 			nombresArray[i] = "Chiquito" + (i+1);
 		}
@@ -11,5 +14,31 @@ public class MascotaApp {
 		for (String variable : nombresArray){
 			System.out.println(variable);
 		}
+
+		// Collections
+		System.out.println("ArrayList:");
+		ArrayList<String> nombresArrayList = new ArrayList<>();
+
+		// Agregar elementos
+		nombresArrayList.add("Chiquito");
+		nombresArrayList.add("Chiquito");
+		nombresArrayList.add("Chiquito");
+		nombresArrayList.add("Chiquito");
+		nombresArrayList.add("Chiquito");
+
+		// Mostrar ArrayList
+		System.out.println(nombresArrayList);
+
+		System.out.println("Tamanio de la ArrayList:");
+		System.out.println(nombresArrayList.size()); // 5
+
+		// Eliminar elemento por index
+		nombresArrayList.remove(4);
+
+		// Eliminar elemento por objeto (En este caso "String")
+		nombresArrayList.remove("Chiquito");
+		System.out.println(nombresArrayList);
+		System.out.println(nombresArrayList.size()); // 3
+
 	}
 }
