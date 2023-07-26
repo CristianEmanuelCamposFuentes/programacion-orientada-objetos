02cabb26861436fdff87d56762f2c358f8640905
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-25 21:18:38 -0300

    Agregar Servicios y Entidades

--- src/Colecciones/Ejercicios/Ejercicio1/Ejercicio/Ejercicio1.java
+++ src/Colecciones/Ejercicios/Ejercicio1/Ejercicio/Ejercicio1.java
@@ -1,19 +1,33 @@
 package Colecciones.Ejercicios.Ejercicio1.Ejercicio;
 
-/*3. Crear una clase llamada Alumno que mantenga información sobre las notas de distintos
-		alumnos. La clase Alumno tendrá como atributos, su nombre y una lista de tipo Integer
-		con sus 3 notas.
-		En el servicio de Alumno deberemos tener un bucle que crea un objeto Alumno. Se pide
-		toda la información al usuario y ese Alumno se guarda en una lista de tipo Alumno y se le
-		pregunta al usuario si quiere crear otro Alumno o no.
-		Después de ese bucle tendremos el siguiente método en el servicio de Alumno:
-		Método notaFinal(): El usuario ingresa el nombre del alumno que quiere calcular su nota
-final y se lo busca en la lista de Alumnos. Si está en la lista, se llama al método. Dentro
-		del método se usará la lista notas para calcular el promedio final de alumno. Siendo este
-		promedio final, devuelto por el método y mostrado en el Ejercicio1.*/
+/*1. Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo String. El
+programa pedirá una raza de perro en un bucle, el mismo se guardará en la lista y
+después se le preguntará al usuario si quiere guardar otro perro o si quiere salir. Si decide
+salir, se mostrará todos los perros guardados en el ArrayList.*/
+
+import Colecciones.Ejercicios.Ejercicio1.Servicios.PerroServicios;
+
+import java.util.Scanner;
 
 public class Ejercicio1 {
+	public static Scanner lector = new Scanner(System.in).useDelimiter("\n");
 	public static void main(String[] args) {
+		PerroServicios PS = new PerroServicios();
+		int opcion;
+
+		System.out.println("Inicio de programa.");
+
+		do {
+			System.out.println("Ingrese la raza: ");
+			PS.agregarRaza();
+
+			opcion = PS.preguntar();
+		} while (opcion != 0);
+
+		System.out.println("Saliendo, tu lista final es: " + PS.razasPerros);
+
+		System.out.println("Programa finalizado.");
+
 
 	}
 }
