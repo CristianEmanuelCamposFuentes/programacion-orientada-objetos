4980e21ae42b8ea50150321a6706bd517d469771
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-08-21 16:44:46 -0300

    Agregar entidad Poliza

---
+++ src/Relaciones/Ejercicios/EjercicioExtra3/Entidades/Poliza.java
@@ -0,0 +1,135 @@
+package Relaciones.Ejercicios.EjercicioExtra3.Entidades;
+
+import java.util.Date;
+
+public class Poliza {
+    private String numeroPoliza;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int cantidadCuotas;
+    private String formaPago;
+    private double montoTotalAsegurado;
+    private boolean incluyeGranizo;
+    private double montoMaximoGranizo;
+    private String tipoCobertura;
+    private Cliente cliente;
+    private Vehiculo vehiculo;
+    private List<Cuota> cuotas;
+
+    public Poliza() {
+    }
+
+    public String getNumeroPoliza() {
+        return numeroPoliza;
+    }
+
+    public void setNumeroPoliza(String numeroPoliza) {
+        this.numeroPoliza = numeroPoliza;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getCantidadCuotas() {
+        return cantidadCuotas;
+    }
+
+    public void setCantidadCuotas(int cantidadCuotas) {
+        this.cantidadCuotas = cantidadCuotas;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public double getMontoTotalAsegurado() {
+        return montoTotalAsegurado;
+    }
+
+    public void setMontoTotalAsegurado(double montoTotalAsegurado) {
+        this.montoTotalAsegurado = montoTotalAsegurado;
+    }
+
+    public boolean isIncluyeGranizo() {
+        return incluyeGranizo;
+    }
+
+    public void setIncluyeGranizo(boolean incluyeGranizo) {
+        this.incluyeGranizo = incluyeGranizo;
+    }
+
+    public double getMontoMaximoGranizo() {
+        return montoMaximoGranizo;
+    }
+
+    public void setMontoMaximoGranizo(double montoMaximoGranizo) {
+        this.montoMaximoGranizo = montoMaximoGranizo;
+    }
+
+    public String getTipoCobertura() {
+        return tipoCobertura;
+    }
+
+    public void setTipoCobertura(String tipoCobertura) {
+        this.tipoCobertura = tipoCobertura;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public List<Cuota> getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(List<Cuota> cuotas) {
+        this.cuotas = cuotas;
+    }
+
+    @Override
+    public String toString() {
+        return "Poliza{" +
+                "numeroPoliza='" + numeroPoliza + '\'' +
+                ", fechaInicio=" + fechaInicio +
+                ", fechaFin=" + fechaFin +
+                ", cantidadCuotas=" + cantidadCuotas +
+                ", formaPago='" + formaPago + '\'' +
+                ", montoTotalAsegurado=" + montoTotalAsegurado +
+                ", incluyeGranizo=" + incluyeGranizo +
+                ", montoMaximoGranizo=" + montoMaximoGranizo +
+                ", tipoCobertura='" + tipoCobertura + '\'' +
+                ", cliente=" + cliente +
+                ", vehiculo=" + vehiculo +
+                ", cuotas=" + cuotas +
+                '}';
+    }
+}
