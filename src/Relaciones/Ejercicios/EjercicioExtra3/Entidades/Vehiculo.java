08a105768a31d1d24c5f302faa8295d2a89df619
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-08-21 16:42:29 -0300

    Agregar entidad Vehiculo

---
+++ src/Relaciones/Ejercicios/EjercicioExtra3/Entidades/Vehiculo.java
@@ -0,0 +1,83 @@
+package Relaciones.Ejercicios.EjercicioExtra3.Entidades;
+
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int anio;
+    private String numeroMotor;
+    private String numeroChasis;
+    private String color;
+    private String tipo;
+
+    public Vehiculo() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getNumeroMotor() {
+        return numeroMotor;
+    }
+
+    public void setNumeroMotor(String numeroMotor) {
+        this.numeroMotor = numeroMotor;
+    }
+
+    public String getNumeroChasis() {
+        return numeroChasis;
+    }
+
+    public void setNumeroChasis(String numeroChasis) {
+        this.numeroChasis = numeroChasis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", anio=" + anio +
+                ", numeroMotor='" + numeroMotor + '\'' +
+                ", numeroChasis='" + numeroChasis + '\'' +
+                ", color='" + color + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
+}
