9c904db092d92176f77167f894eca44383ddf2e5
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-08-31 00:02:43 -0300

    Agregar instancias de Animales y metodos alimentarse

--- src/Herencias/Ejercicios/Ejercicio1/Ejercicio/Main.java
+++ src/Herencias/Ejercicios/Ejercicio1/Ejercicio/Main.java
@@ -1,6 +1,28 @@
 package Herencias.Ejercicios.Ejercicio1.Ejercicio;
 
+import Herencias.Ejercicios.Ejercicio1.Entidades.Animal;
+import Herencias.Ejercicios.Ejercicio1.Entidades.Caballo;
+import Herencias.Ejercicios.Ejercicio1.Entidades.Perro;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Inicio del programa: ");
+        // Declaracion del objeto Perro
+        Animal perro1 = new Perro("Stitch", "Carne", 15, "Doberman");
+        perro1.alimentarse();
+
+        // Declaracion de otro objeto Perro
+        Animal perro2 = new Perro("Teddy", "Croquetas", 10, "Chihuahua");
+        perro2.alimentarse();
+
+        // Declaracion del objeto Gato
+        Animal gato1 = new Animal("Pelusa", "Galletas", 15, "Siamés");
+        gato1.alimentarse();
+
+        // Declaracion del objeto Caballo
+        Animal caballo1 = new Caballo("Spark", "Pasto", 25, "Fino");
+        caballo1.alimentarse();
+
+        System.out.println("Fin del programa. ");
     }
 }
