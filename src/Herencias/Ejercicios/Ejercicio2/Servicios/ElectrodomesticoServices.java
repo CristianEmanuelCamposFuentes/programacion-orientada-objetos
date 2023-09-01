b2133555b396396f3c9980e996edb733372bc607
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-08-31 22:06:36 -0300

    Agregar metodo comprobarConsumoEnergetico()

--- src/Herencias/Ejercicios/Ejercicio2/Servicios/ElectrodomesticoServices.java
+++ src/Herencias/Ejercicios/Ejercicio2/Servicios/ElectrodomesticoServices.java
@@ -1,6 +1,10 @@
 package Herencias.Ejercicios.Ejercicio2.Servicios;
 
 public class ElectrodomesticoServices {
-
-
+    public char comprobarConsumoEnergetico(char c) {
+        if ((c >= 'A') && (c <= 'F')) {
+                return c;
+        }
+        return 'F';
+    }
 }
