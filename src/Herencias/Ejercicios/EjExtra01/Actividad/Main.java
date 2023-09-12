07e45e6f8584eb4eb9c77a4c97e8ad16bbf6b386
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-09-12 20:17:11 -0300

    Agregar opciones de crud en el menu del Main

--- src/Herencias/Ejercicios/EjExtra01/Actividad/Main.java
+++ src/Herencias/Ejercicios/EjExtra01/Actividad/Main.java
@@ -27,28 +27,46 @@ precio final de su alquiler.
 public class Main {
     public static final Scanner scanner = new Scanner(System.in).useDelimiter("\n");
     public static void main(String[] args) {
+        ServicioAlquiler SA = new ServicioAlquiler();
+        System.out.println("Inicio del programa:");
 
         while (true) {
             System.out.println("Menú de Alquiler de Barcos");
             System.out.println("1. Alquilar Barco a Motor");
             System.out.println("2. Alquilar Velero");
             System.out.println("3. Alquilar Yate de Lujo");
-            System.out.println("4. Salir");
+            System.out.println("4. Agregar Nuevo Barco");
+            System.out.println("5. Mostrar Lista de Botes Disponibles");
+            System.out.println("6. Actualizar Información de Barco");
+            System.out.println("7. Eliminar Barco de la Lista");
+            System.out.println("8. Salir");
 
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
 
             switch (opcion) {
                 case 1:
-                    ServicioAlquiler.alquilarBarcoMotor();
+                    SA.alquilarBarcoMotor();
                     break;
                 case 2:
-                    ServicioAlquiler.alquilarVelero();
+                    SA.alquilarVelero();
                     break;
                 case 3:
-                    ServicioAlquiler.alquilarYateLujo();
+                    SA.alquilarYateLujo();
                     break;
                 case 4:
+                    SA.agregarBarco();
+                    break;
+                case 5:
+                    SA.mostrarBotes();
+                    break;
+                case 6:
+                    SA.actualizarBarco();
+                    break;
+                case 7:
+                    SA.eliminarBarco();
+                    break;
+                case 8:
                     System.out.println("Gracias por utilizar nuestro servicio de alquiler.");
                     scanner.close();
                     return;
@@ -56,5 +74,4 @@ public class Main {
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
             }
         }
-    }
 }
