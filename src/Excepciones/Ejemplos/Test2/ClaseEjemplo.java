3f9a9eeb063ac66a791cab255d9916817828ebd0
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-09-17 10:58:02 -0300

    Inicio de ejemplo Test2

---
+++ src/Excepciones/Ejemplos/Test2/ClaseEjemplo.java
@@ -0,0 +1,25 @@
+package Excepciones.Ejemplos.Test2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ClaseEjemplo {
+    public static void main(String[] args) {
+        int x = 10, y, z;
+        System.out.println("Ingrese y: ");
+
+        try {
+            y = new Scanner(System.in).nextInt();
+            z = x / y;
+            System.out.println("Resultado: " + z);
+        } catch (InputMismatchException ime) {
+            System.out.println("No es un entero !");
+        } catch (ArithmeticException ae) {
+            System.out.println("No se puede dividir por cero !");
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        System.out.println("Fin del programa.");
+    }
+}
