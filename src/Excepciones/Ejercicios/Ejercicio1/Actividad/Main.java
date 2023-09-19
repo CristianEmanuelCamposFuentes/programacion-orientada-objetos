22912a148c8525a08539d63bfcc07330755f5939
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-09-19 19:21:56 -0300

    Agregar consigna

--- src/Excepciones/Ejercicios/Ejercicio1/Actividad/Main.java
+++ src/Excepciones/Ejercicios/Ejercicio1/Actividad/Main.java
@@ -4,6 +4,12 @@ import Excepciones.Ejercicios.Ejercicio1.Entidades.Persona;
 import Excepciones.Ejercicios.Ejercicio1.Servicios.MenorDeEdadException;
 import Excepciones.Ejercicios.Ejercicio1.Servicios.PersonaServices;
 
+/*
+* Inicializar un objeto de la clase Persona ejercicio 3 de la guía 8 Servicios, a null y tratar
+de invocar el método esMayorDeEdad() a través de ese objeto. Luego, englobe el código
+con una cláusula try-catch para probar la nueva excepción que debe ser controlada.
+* */
+
 public class Main {
     public static void main(String[] args) {
         PersonaServices PS = new PersonaServices();
